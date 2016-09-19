b0054d8e8d24ca73e7601e9a5dc7f1be4dd48a94
cpeppers <chadpeppers2@gmail.com>
2016-09-19 12:12:18 -0500

    fixed required statement and various code cleanup.

--- composition.php
+++ composition.php
@@ -1,6 +1,14 @@
 <?php
-require($_SERVER['DOCUMENT_ROOT'] . '/vendor/autoload.php');
 
-$duck = new Patterns\Composition\Duck\MallardDuck();
+/**
+ * @file
+ * Example of Strategy Pattern using the Duck example.
+ */
+
+require $_SERVER['DOCUMENT_ROOT'] . '/vendor/autoload.php';
+
+use Patterns\Composition\Duck\MallardDuck;
+
+$duck = new MallardDuck();
 echo $duck->performQuack() . '<br/>';
 echo $duck->performFly();
